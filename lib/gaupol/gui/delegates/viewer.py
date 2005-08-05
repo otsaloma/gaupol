dcf45481d40d9e7e458ed1037c69ef85aa2417d6
Osmo Salomaa <otsaloma@cc.hut.fi>
2005-08-05 13:20:45 +0000

    More code clean-up.

--- lib/gaupol/gui/delegates/viewer.py
+++ lib/gaupol/gui/delegates/viewer.py
@@ -214,3 +214,30 @@ class Viewer(Delegate):
         if event.button == 3:
             menu = self.uim.get_widget('/ui/column_popup')
             menu.popup(None, None, None, event.button, event.time)
+
+    def on_invert_selection_activated(self, *args):
+        """Invert current selection."""
+
+        project = self.get_current_project()
+        store = project.tree_view.get_model()
+        selection = project.tree_view.get_selection()
+        
+        rows = selection.get_selected_rows()[1]
+        selection.select_all()
+        
+        for row in rows:
+            selection.unselect_path(row)
+        
+    def on_select_all_activated(self, *args):
+        """Select all subtitles."""
+
+        project = self.get_current_project()
+        selection = project.tree_view.get_selection()
+        selection.select_all()
+        
+    def on_unselect_all_activated(self, *args):
+        """Unselect all subtitles."""
+
+        project = self.get_current_project()
+        selection = project.tree_view.get_selection()
+        selection.unselect_all()
