def27e66b9a9545365fd4f5ef851557e57a0d3ef
Osmo Salomaa <otsaloma@cc.hut.fi>
2005-08-30 18:28:56 +0000

    2005-08-30  Osmo Salomaa  <otsaloma@cc.hut.fi>
    	* gaupol (prepare_gettext):
    	  Added dgettext and removed broken ngettext for now.
    
    	* gui/dialogs/language.py:
    	  language-dialog.glade:
    	  Reformatted language lists.
    
    	* lib/util/langlib.py:
    	  Rewrote module based on iso-codes project.

--- lib/gaupol/gui/dialogs/language.py
+++ lib/gaupol/gui/dialogs/language.py
@@ -20,6 +20,8 @@
 """Dialog for selecting language and target for spell-check."""
 
 
+import logging
+
 try:
     from psyco.classes import *
 except ImportError:
@@ -33,15 +35,7 @@ from gaupol.gui.util import gui
 from gaupol.lib.util import langlib
 
 
-LANGS = enchant.list_languages()
-
-# List of language descriptive names in sanme order as LANGS.
-LANG_DESC_NAMES = []
-for lang in LANGS:
-    try:
-        LANG_DESC_NAMES.append(langlib.get_descriptive_name(lang))
-    except KeyError:
-        LANG_DESC_NAMES.append(lang)
+logger = logging.getLogger()
 
 
 class LanguageDialog(object):
@@ -78,6 +72,16 @@ class LanguageDialog(object):
         tran_label = w('language_translation_label')
         tran_label.set_mnemonic_widget(self._lang_tran_tree_view)
         
+        self._lang_codes = []
+        self._lang_names = []
+        self._countries  = []
+        self._providers  = []
+        
+        # Full language names: "language-country" to get alphabetical order
+        # correct
+        self._full_names = []
+        
+        self._list_languages()
         self._fill_language_tree_view(self._lang_text_tree_view)
         self._fill_language_tree_view(self._lang_tran_tree_view)
 
@@ -92,15 +96,36 @@ class LanguageDialog(object):
         model = gtk.ListStore(gobject.TYPE_STRING)
         tree_view.set_model(model)
         tree_view.set_headers_visible(False)
+        
+        def render_text(tree_view_column, cell_renderer, model, tree_iter):
+            """Render text in the cell."""
+
+            full_name = model.get_value(tree_iter, 0)
+            index = self._full_names.index(full_name)
+            markup = ''
+            
+            if self._lang_names[index] is not None:
+                markup += '<b>%s</b>' % self._lang_names[index]
+                
+            if self._countries[index] is not None:
+                markup += '\n%s' % self._countries[index]
+            
+            # TRANSLATORS: Spell-check dictionary, e.g. "aspell: en_US".
+            details = self._providers[index], self._lang_codes[index]
+            detail = _('%s: %s') % details
+            markup += '\n<small>%s</small>' % detail
+            
+            cell_renderer.set_property('markup', markup)
 
         cell_renderer = gtk.CellRendererText()
-        tree_view_column = gtk.TreeViewColumn('', cell_renderer, text=0)
+        tree_view_column = gtk.TreeViewColumn('', cell_renderer)
+        tree_view_column.set_cell_data_func(cell_renderer, render_text)
         tree_view.append_column(tree_view_column)
         
         # Add available languages to list.
-        desc_names = LANG_DESC_NAMES[:]
-        desc_names.sort()
-        for name in desc_names:
+        names = self._full_names[:]
+        names.sort()
+        for name in names:
             model.append([name])
             
         selection = tree_view.get_selection()
@@ -131,11 +156,10 @@ class LanguageDialog(object):
             return None
         else:
             row = rows[0]
-            
-        desc_name = model[row][0]
-        index = LANG_DESC_NAMES.index(desc_name)
         
-        return LANGS[index]
+        full_name = model[row][0]
+        index = self._full_names.index(full_name)
+        return self._lang_codes[index]
         
     def get_text_language(self):
         """Get language set for text column."""
@@ -150,6 +174,45 @@ class LanguageDialog(object):
         selection = self._lang_tran_tree_view.get_selection()
         
         return self._get_language(selection)
+        
+    def _list_languages(self):
+        """Create lists of available languages."""
+
+        self._lang_codes = enchant.list_languages()
+        broker = enchant.Broker()
+        
+        # Get providers.
+        for code in self._lang_codes:
+        
+            try:
+                self._providers.append(broker.request_dict(code).provider.name)
+            except enchant.Error, detail:
+                logger.error(detail)
+                self._providers.append(None)
+    
+        # Remove all languages that failed provider check.
+        for i in reversed(range(len(self._providers))):
+            if self._providers[i] is None:
+                self._providers.pop(i)
+                self._lang_codes.pop(i)
+        
+        # Get languages names and countries.
+        for code in self._lang_codes:
+        
+            try:
+                self._lang_names.append(langlib.get_language(code))
+            except KeyError:
+                self._lang_names.append(None)
+                
+            try:
+                self._countries.append(langlib.get_country(code))
+            except KeyError:
+                self._countries.append(None)
+        
+        # Get full names.
+        for i in range(len(self._lang_codes)):
+            entry = '%s-%s' % (self._lang_names[i], self._countries[i])
+            self._full_names.append(entry)
         
     def run(self):
         """Show and run the dialog."""
@@ -180,35 +243,35 @@ class LanguageDialog(object):
         
         # Try language.
         try:
-            index = LANGS.index(lang)
+            index = self._lang_codes.index(lang)
         except ValueError:
         
             # Try language with only first two letters.
             try:
-                index = LANGS.index(lang[:2])
+                index = self._lang_codes.index(lang[:2])
             except (TypeError, ValueError):
         
                 # Try default language.
                 try:
                     lang = enchant.Dict().tag
                     try:
-                        index = LANGS.index(lang)
+                        index = self._lang_codes.index(lang)
                     except ValueError:
                     
                         # Try default language with only first two letters.
                         try:
-                            index = LANGS.index(lang[:2])
+                            index = self._lang_codes.index(lang[:2])
                         except ValueError:
                             return
                 except enchant.Error:
                     return
-            
-        desc_name = LANG_DESC_NAMES[index]
+        
+        full_name = self._full_names[index]
         
         for i in range(len(model)):
-            if model[i][0] == desc_name:
+            if model[i][0] == full_name:
                 selection.select_path(i)
-                return
+                break
         
     def set_text_language(self, lang):
         """Set language for text column."""
