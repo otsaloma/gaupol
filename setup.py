05fe12e57b0496347a765e643f538e0ba1b0175d
Osmo Salomaa <otsaloma@cc.hut.fi>
2006-01-24 17:02:08 +0000

    2006-01-24  Osmo Salomaa  <otsaloma@cc.hut.fi>
    	* MANIFEST.in:
    	* setup.py:
    	  Removed doc/formats/* from tarballs and install.
    
    	* doc/formats/ass-specs.rtf:
    	* doc/formats/ass.txt:
    	  New files.

--- setup.py
+++ setup.py
@@ -57,14 +57,12 @@ doc_files = (
     'README.translators',
     'TODO'
 )
-spec_files = glob.glob(os.path.join('doc', 'formats', '*'))
 
 data_files = [
     ('share/gaupol/glade'      , glade_files               ),
     ('share/gaupol/icons'      , icon_files                ),
     ('share/gaupol/ui'         , ui_files                  ),
     ('share/doc/gaupol'        , doc_files                 ),
-    ('share/doc/gaupol/formats', spec_files                ),
     ('share/applications'      , ['gaupol.desktop']        ),
     ('share/pixmaps'           , ['data/icons/gaupol.png'] ),
 ]
