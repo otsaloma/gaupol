9ec072555c3b32959e022882f89d93fbd77ad8b0
Osmo Salomaa <otsaloma@cc.hut.fi>
2007-07-02 23:02:54 +0000

    2007-07-02  Osmo Salomaa  <otsaloma@cc.hut.fi>
    	* COPYING:
    	* gaupol/*.py:
    	* gaupol/gtk/dialogs/about.py:
    	Upgrade license to GPL v3 or later.

--- gaupol/gtk/main.py
+++ gaupol/gtk/main.py
@@ -2,9 +2,9 @@
 #
 # This file is part of Gaupol.
 #
-# Gaupol is free software; you can redistribute it and/or modify it under the
+# Gaupol is free software: you can redistribute it and/or modify it under the
 # terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later
+# Foundation, either version 3 of the License, or (at your option) any later
 # version.
 #
 # Gaupol is distributed in the hope that it will be useful, but WITHOUT ANY
@@ -12,8 +12,7 @@
 # A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License along with
-# Gaupol; if not, write to the Free Software Foundation, Inc., 51 Franklin
-# Street, Fifth Floor, Boston, MA 02110-1301, USA.
+# Gaupol.  If not, see <http://www.gnu.org/licenses/>.
 
 
 """GTK user interface initialization."""
