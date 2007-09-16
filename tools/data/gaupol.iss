90c1fb9f1137aa8044b789f885ffb688239665e0
Osmo Salomaa <otsaloma@cc.hut.fi>
2007-09-16 16:11:40 +0000

    2007-09-16  Osmo Salomaa  <otsaloma@cc.hut.fi>
    	* tools/data/gaupol.iss:
    	Remove EULAish LicenseFile field.
    
    	* tools/py2exe.bat:
    	Remove unused iso-codes .mo files.

--- tools/data/gaupol.iss
+++ tools/data/gaupol.iss
@@ -3,12 +3,11 @@ AllowNoIcons=yes
 AppName=Gaupol
 AppPublisher=Osmo Salomaa
 AppPublisherURL=http://home.gna.org/gaupol/
-AppVerName=Gaupol 0.10
+AppVerName=Gaupol 0.11
 Compression=lzma
 DefaultDirName={pf}\Gaupol
 DefaultGroupName=Gaupol
-LicenseFile=F:\gaupol\COPYING
-OutputBaseFilename=gaupol-0.10-win32
+OutputBaseFilename=gaupol-0.11-win32
 OutputDir=F:\gaupol\win32
 SolidCompression=yes
 
--- tools/py2exe.bat
+++ tools/py2exe.bat
@@ -1,21 +1,25 @@
-@set GTKDIR=F:\GTK
-@set PYDIR=F:\Python25
-@cd ..
-@rmdir /S /Q build
-@rmdir /S /Q dist
-@mkdir dist\etc
-@mkdir dist\lib
-@mkdir dist\share
-@%PYDIR%\python.exe setup.py py2exe
-@pause
-@xcopy %GTKDIR%\bin\*.dll dist /E /Y
-@xcopy %GTKDIR%\etc dist\etc /E
-@xcopy %GTKDIR%\lib dist\lib /E
-@xcopy %GTKDIR%\share dist\share /E
-@xcopy %PYDIR%\Lib\site-packages\enchant\lib dist\lib /E
-@xcopy %PYDIR%\Lib\site-packages\enchant\share dist\share /E
-@rmdir /S /Q dist\share\applications
-@rmdir /S /Q dist\share\gtk-2.0
-@rmdir /S /Q dist\share\gtkthemeselector
-@rmdir /S /Q dist\share\man
-@pause
+echo off
+set GTKDIR=F:\GTK
+set PYDIR=F:\Python25
+cd ..
+rmdir /S /Q build
+rmdir /S /Q dist
+mkdir dist\etc
+mkdir dist\lib
+mkdir dist\share
+%PYDIR%\python.exe setup.py py2exe
+pause
+xcopy %GTKDIR%\bin\*.dll dist /E /Y
+xcopy %GTKDIR%\etc dist\etc /E
+xcopy %GTKDIR%\lib dist\lib /E
+xcopy %GTKDIR%\share dist\share /E
+xcopy %PYDIR%\Lib\site-packages\enchant\lib dist\lib /E
+xcopy %PYDIR%\Lib\site-packages\enchant\share dist\share /E
+rmdir /S /Q dist\share\applications
+rmdir /S /Q dist\share\gtk-2.0
+rmdir /S /Q dist\share\gtkthemeselector
+rmdir /S /Q dist\share\man
+for /R dist\share\locale %%f in (iso_3166_2.mo*) do del /Q %%f
+for /R dist\share\locale %%f in (iso_4217.mo*) do del /Q %%f
+for /R dist\share\locale %%f in (iso_639_3.mo*) do del /Q %%f
+pause
