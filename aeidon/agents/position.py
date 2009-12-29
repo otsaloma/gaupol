b7a92bbf6937d57fd04d44e9db2a25d210829e59
Osmo Salomaa <otsaloma@cc.hut.fi>
2009-12-29 04:18:41 +0200

    Port agents.position to aeidon.

--- aeidon/agents/position.py
+++ aeidon/agents/position.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2005-2007 Osmo Salomaa
+# Copyright (C) 2005-2007,2009 Osmo Salomaa
 #
 # This file is part of Gaupol.
 #
@@ -26,85 +26,94 @@ class PositionAgent(aeidon.Delegate):
 
     """Manipulating times and frames."""
 
+    # pylint: disable-msg=E0203,E1101,W0201
+
     __metaclass__ = aeidon.Contractual
 
-    def _get_frame_transform_ensure(self, value, point_1, point_2):
+    def _get_frame_transform_ensure(self, value, p1, p2):
         assert isinstance(value[1], int)
 
-    def _get_frame_transform(self, point_1, point_2):
-        """Return a formula for linear correction of positions.
+    def _get_frame_transform(self, p1, p2):
+        """Return a formula for linear transformation of positions.
 
-        point_* should be a tuple of index and start frame.
-        Return coefficient and constant, with which all subtitles should be
-        scaled and shifted by to apply correction.
+        Return coefficient and constant, with which all subtitles
+        should be scaled and shifted by to apply correction.
         """
         # Think of this as a linear transformation where input positions
         # are located on the x-axis and output positions on the y-axis.
-        x_1 = self.subtitles[point_1[0]].start_frame
-        x_2 = self.subtitles[point_2[0]].start_frame
-        y_1 = point_1[1]
-        y_2 = point_2[1]
-        coefficient = (y_2 - y_1) / (x_2 - x_1)
-        constant = int(round(- (coefficient * x_1) + y_1, 0))
+        x1 = self.subtitles[p1[0]].start_frame
+        x2 = self.subtitles[p2[0]].start_frame
+        y1 = p1[1]
+        y2 = p2[1]
+        coefficient = (y2 - y1) / (x2 - x1)
+        constant = int(round(- (coefficient * x1) + y1, 0))
         return coefficient, constant
 
-    def _get_seconds_transform_ensure(self, value, point_1, point_2):
+    def _get_seconds_transform_ensure(self, value, p1, p2):
         assert isinstance(value[1], float)
 
-    def _get_seconds_transform(self, point_1, point_2):
-        """Return a formula for linear correction of positions.
+    def _get_seconds_transform(self, p1, p2):
+        """Return a formula for linear transformation of positions.
 
-        point_* should be a tuple of index and start seconds.
         Return coefficient and constant, with which all subtitles should be
         scaled and shifted by to apply correction.
         """
         # Think of this as a linear transformation where input positions
         # are located on the x-axis and output positions on the y-axis.
-        x_1 = self.subtitles[point_1[0]].start_seconds
-        x_2 = self.subtitles[point_2[0]].start_seconds
-        y_1 = point_1[1]
-        y_2 = point_2[1]
-        coefficient = (y_2 - y_1) / (x_2 - x_1)
-        constant = - (coefficient * x_1) + y_1
+        x1 = self.subtitles[p1[0]].start_seconds
+        x2 = self.subtitles[p2[0]].start_seconds
+        y1 = p1[1]
+        y2 = p2[1]
+        coefficient = (y2 - y1) / (x2 - x1)
+        constant = - (coefficient * x1) + y1
         return coefficient, constant
 
-    def _get_time_transform(self, point_1, point_2):
+    def _get_time_transform(self, p1, p2):
         """Return a formula for linear correction of positions.
 
-        point_* should be a tuple of index and start time.
         Return coefficient and constant, with which all subtitles should be
         scaled and shifted by to apply correction.
         """
-        point_1 = list(point_1)
-        point_1[1] = self.calc.time_to_seconds(point_1[1])
-        point_2 = list(point_2)
-        point_2[1] = self.calc.time_to_seconds(point_2[1])
-        return self._get_seconds_transform(point_1, point_2)
+        p1 = [p1[0], self.calc.time_to_seconds(p1[1])]
+        p2 = [p2[0], self.calc.time_to_seconds(p2[1])]
+        return self._get_seconds_transform(p1, p2)
 
     def adjust_durations_require(self, indices=None, *args, **kwargs):
         for index in indices or []:
             assert 0 <= index < len(self.subtitles)
 
     @aeidon.deco.revertable
-    def adjust_durations(self, indices=None, optimal=None, lengthen=False,
-        shorten=False, maximum=None, minimum=None, gap=None, register=-1):
-        """Lengthen or shorten durations by changing the end positions.
+    def adjust_durations(self,
+                         indices=None,
+                         optimal=None,
+                         lengthen=False,
+                         shorten=False,
+                         minimum=None,
+                         maximum=None,
+                         gap=None,
+                         register=-1):
+        """Lengthen or shorten durations by changing end positions.
 
-        indices can be None to process all subtitles.
-        optimal is duration per character in seconds.
-        gap is seconds to be left between consecutive subtitles.
+        `indices` can be ``None`` to process all subtitles. `optimal` is
+        duration per character in seconds. `lengthen` is ``True`` to increase
+        durations if shorter than optimal. `shorten` is ``True`` to decrease
+        durations if longer than optimal. `maximum` is the greatest allowed
+        duration in seconds. `minimum` is the smallest allowed duration in
+        seconds. `gap` is seconds to be left between consecutive subtitles.
+        Using a gap of zero (or a bit more) is always a good idea if dealing
+        with subtitles where overlapping is not desirable.
+
         Return changed indices.
         """
         new_indices = []
         new_subtitles = []
-        indices = indices or range(len(self.subtitles))
-        for index in indices:
+        for index in indices or range(len(self.subtitles)):
             start = self.subtitles[index].start_seconds
             end = self.subtitles[index].end_seconds
-            end_max = 360000
-            if index < (len(self.subtitles) - 1):
-                end_max = self.subtitles[index + 1].start_seconds
-            if optimal is not None:
+            end_max = (self.subtitles[index + 1].start_seconds if
+                       index < (len(self.subtitles) - 1) else 360000)
+
+            if (optimal is not None) and (lengthen or shorten):
                 length = self.get_text_length(index, aeidon.documents.MAIN)
                 optimal_duration = optimal * length
                 if ((end - start) < optimal_duration) and lengthen:
@@ -122,45 +131,47 @@ class PositionAgent(aeidon.Delegate):
                 subtitle = self.subtitles[index].copy()
                 subtitle.end = end
                 new_subtitles.append(subtitle)
-
         if new_indices:
-            method = self.replace_positions
-            method(new_indices, new_subtitles, register=register)
+            self.replace_positions(new_indices,
+                                   new_subtitles,
+                                   register=register)
+
             self.set_action_description(register, _("Adjusting durations"))
         return new_indices
 
     def convert_framerate_require(self, indices, *args, **kwargs):
-        for index in indices or []:
+        for index in indices or ():
             assert 0 <= index < len(self.subtitles)
 
     @aeidon.deco.revertable
-    def convert_framerate(self, indices, input, output, register=-1):
-        """Set the value of the framerate and convert subtitles to it.
+    def convert_framerate(self,
+                          indices,
+                          framerate_in,
+                          framerate_out,
+                          register=-1):
+        """Set the value of framerate and convert subtitles to it.
 
-        indices can be None to process all subtitles.
+        `indices` can be ``None`` to process all subtitles.
         """
-        self.set_framerate(input, register=None)
+        self.set_framerate(framerate_in, register=None)
         new_subtitles = []
         indices = indices or range(len(self.subtitles))
         for index in indices:
             subtitle = self.subtitles[index].copy()
-            subtitle.convert_framerate(output)
+            subtitle.convert_framerate(framerate_out)
             new_subtitles.append(subtitle)
-
-        self.set_framerate(output)
+        self.set_framerate(framerate_out)
         self.replace_positions(indices, new_subtitles, register=register)
         self.group_actions(register, 2, _("Converting framerate"))
 
     @aeidon.deco.revertable
     def set_framerate(self, framerate, register=-1):
-        """Set the value of the framerate."""
-
+        """Set the value of framerate."""
         orig_framerate = self.framerate
         self.framerate = framerate
         self.calc = aeidon.Calculator(framerate)
         for subtitle in self.subtitles:
             subtitle.framerate = framerate
-
         action = self.new_revertable_action(register)
         action.docs = tuple(aeidon.documents)
         action.description = _("Setting framerate")
@@ -169,14 +180,15 @@ class PositionAgent(aeidon.Delegate):
         self.register_action(action)
 
     def shift_positions_require(self, indices, count, register=-1):
-        for index in indices or []:
+        for index in indices or ():
             assert 0 <= index < len(self.subtitles)
 
     @aeidon.deco.revertable
     def shift_positions(self, indices, value, register=-1):
         """Make subtitles appear earlier or later.
 
-        indices can be None to process all subtitles.
+        `indices` can be ``None`` to process all subtitles. `value` should be a
+        string for time, a float for seconds or an integer for frames.
         """
         new_subtitles = []
         indices = indices or range(len(self.subtitles))
@@ -184,28 +196,28 @@ class PositionAgent(aeidon.Delegate):
             subtitle = self.subtitles[index].copy()
             subtitle.shift_positions(value)
             new_subtitles.append(subtitle)
-
         self.replace_positions(indices, new_subtitles, register=register)
         self.set_action_description(register, _("Shifting positions"))
 
     def transform_positions_require(self, indices, *args, **kwargs):
-        for index in indices or []:
+        for index in indices or ():
             assert 0 <= index < len(self.subtitles)
 
     @aeidon.deco.revertable
-    def transform_positions(self, indices, point_1, point_2, register=-1):
-        """Change positions by linear two-point correction.
+    def transform_positions(self, indices, p1, p2, register=-1):
+        """Change positions by linear two-point corrections.
 
-        indices can be None to process all subtitles.
-        point_* should be a tuple of index and start position.
+        `indices` can be ``None`` to process all subtitles. `p1` and `p2`
+        should be tuples of index, position, where position should be a string
+        for time, a float for seconds or an integer for frames.
         """
-        if isinstance(point_1[1], basestring):
-            get_transform = self._get_time_transform
-        elif isinstance(point_1[1], int):
-            get_transform = self._get_frame_transform
-        elif isinstance(point_1[1], float):
-            get_transform = self._get_seconds_transform
-        coefficient, constant = get_transform(point_1, point_2)
+        if isinstance(p1[1], basestring):
+            method = self._get_time_transform
+        if isinstance(p1[1], int):
+            method = self._get_frame_transform
+        if isinstance(p1[1], float):
+            method = self._get_seconds_transform
+        coefficient, constant = method(p1, p2)
         new_subtitles = []
         indices = indices or range(len(self.subtitles))
         for index in indices:
@@ -213,6 +225,5 @@ class PositionAgent(aeidon.Delegate):
             subtitle.scale_positions(coefficient)
             subtitle.shift_positions(constant)
             new_subtitles.append(subtitle)
-
         self.replace_positions(indices, new_subtitles, register=register)
         self.set_action_description(register, _("Transforming positions"))
--- aeidon/agents/test/test_position.py
+++ aeidon/agents/test/test_position.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2005-2007 Osmo Salomaa
+# Copyright (C) 2005-2007,2009 Osmo Salomaa
 #
 # This file is part of Gaupol.
 #
@@ -14,25 +14,24 @@
 # You should have received a copy of the GNU General Public License along with
 # Gaupol. If not, see <http://www.gnu.org/licenses/>.
 
-import gaupol
+import aeidon
 
 
-class TestPositionAgent(gaupol.TestCase):
+class TestPositionAgent(aeidon.TestCase):
 
     def setup_method(self, method):
-
         self.project = self.new_project()
 
     def test_adjust_durations__gap(self):
-
         subtitles = self.project.subtitles
         subtitles[0].start = "00:00:01.000"
         subtitles[0].end = "00:00:02.000"
         subtitles[1].start = "00:00:02.000"
         subtitles[1].end = "00:00:03.000"
         subtitles[2].start = "00:00:04.000"
-        self.project.adjust_durations(
-            indices=range(len(self.project.subtitles)), gap=0.3)
+        self.project.adjust_durations(range(len(self.project.subtitles)),
+                                      gap=0.3)
+
         assert subtitles[0].start == "00:00:01.000"
         assert subtitles[0].end == "00:00:01.700"
         assert subtitles[1].start == "00:00:02.000"
@@ -40,7 +39,6 @@ class TestPositionAgent(gaupol.TestCase):
         assert subtitles[2].start == "00:00:04.000"
 
     def test_adjust_durations__lengthen(self):
-
         subtitles = self.project.subtitles
         subtitles[0].start = "00:00:01.000"
         subtitles[0].end = "00:00:01.100"
@@ -48,43 +46,48 @@ class TestPositionAgent(gaupol.TestCase):
         subtitles[1].start = "00:00:02.000"
         subtitles[1].end = "00:00:02.100"
         subtitles[1].main_text = "12345678901234567890"
-        self.project.adjust_durations(
-            indices=None, optimal=0.05, lengthen=True)
+        self.project.adjust_durations(None,
+                                      optimal=0.05,
+                                      lengthen=True)
+
         assert subtitles[0].start == "00:00:01.000"
         assert subtitles[0].end == "00:00:01.500"
         assert subtitles[1].start == "00:00:02.000"
         assert subtitles[1].end == "00:00:03.000"
 
     def test_adjust_durations__maximum(self):
-
         subtitles = self.project.subtitles
         subtitles[0].start = "00:00:01.000"
         subtitles[0].end = "00:00:01.100"
         subtitles[1].start = "00:00:02.000"
         subtitles[1].end = "00:00:02.100"
-        self.project.adjust_durations(
-            indices=None, optimal=0.1, lengthen=True, maximum=0.5)
+        self.project.adjust_durations(None,
+                                      optimal=0.1,
+                                      lengthen=True,
+                                      maximum=0.5)
+
         assert subtitles[0].start == "00:00:01.000"
         assert subtitles[0].end == "00:00:01.500"
         assert subtitles[1].start == "00:00:02.000"
         assert subtitles[1].end == "00:00:02.500"
 
     def test_adjust_durations__minimum(self):
-
         subtitles = self.project.subtitles
         subtitles[0].start = "00:00:01.000"
         subtitles[0].end = "00:00:01.900"
         subtitles[1].start = "00:00:02.000"
         subtitles[1].end = "00:00:02.900"
-        self.project.adjust_durations(
-            indices=None, optimal=0.001, shorten=True, minimum=0.5)
+        self.project.adjust_durations(None,
+                                      optimal=0.001,
+                                      shorten=True,
+                                      minimum=0.5)
+
         assert subtitles[0].start == "00:00:01.000"
         assert subtitles[0].end == "00:00:01.500"
         assert subtitles[1].start == "00:00:02.000"
         assert subtitles[1].end == "00:00:02.500"
 
     def test_adjust_durations__shorten(self):
-
         subtitles = self.project.subtitles
         subtitles[0].start = "00:00:01.000"
         subtitles[0].end = "00:00:01.900"
@@ -92,53 +95,51 @@ class TestPositionAgent(gaupol.TestCase):
         subtitles[1].start = "00:00:02.000"
         subtitles[1].end = "00:00:02.900"
         subtitles[1].main_text = "12345678901234567890"
-        self.project.adjust_durations(
-            indices=None, optimal=0.01, shorten=True)
+        self.project.adjust_durations(None,
+                                      optimal=0.01,
+                                      shorten=True)
+
         assert subtitles[0].start == "00:00:01.000"
         assert subtitles[0].end == "00:00:01.100"
         assert subtitles[1].start == "00:00:02.000"
         assert subtitles[1].end == "00:00:02.200"
 
     def test_convert_framerate__frame(self):
-
         self.project.open_main(self.new_microdvd_file(), "ascii")
         self.project.subtitles[0].start = 100
         self.project.subtitles[1].start = 200
-        input = gaupol.framerates.FPS_24
-        output = gaupol.framerates.FPS_25
-        indices = range(len(self.project.subtitles))
-        self.project.convert_framerate(indices, input, output)
-        assert self.project.framerate == output
+        self.project.convert_framerate(range(len(self.project.subtitles)),
+                                       aeidon.framerates.FPS_24,
+                                       aeidon.framerates.FPS_25)
+
+        assert self.project.framerate == aeidon.framerates.FPS_25
         for subtitle in self.project.subtitles:
-            assert subtitle.framerate == output
+            assert subtitle.framerate == aeidon.framerates.FPS_25
         assert self.project.subtitles[0].start == 104
         assert self.project.subtitles[1].start == 209
 
     def test_convert_framerate__time(self):
-
         self.project.open_main(self.new_subrip_file(), "ascii")
         self.project.subtitles[0].start = "00:00:01.000"
         self.project.subtitles[1].start = "00:00:02.000"
-        input = gaupol.framerates.FPS_24
-        output = gaupol.framerates.FPS_25
-        self.project.convert_framerate(None, input, output)
-        assert self.project.framerate == output
+        self.project.convert_framerate(None,
+                                       aeidon.framerates.FPS_24,
+                                       aeidon.framerates.FPS_25)
+
+        assert self.project.framerate == aeidon.framerates.FPS_25
         for subtitle in self.project.subtitles:
-            assert subtitle.framerate == output
+            assert subtitle.framerate == aeidon.framerates.FPS_25
         assert self.project.subtitles[0].start == "00:00:00.959"
         assert self.project.subtitles[1].start == "00:00:01.918"
 
     def test_set_framerate(self):
-
-        framerate = gaupol.framerates.FPS_25
-        self.project.set_framerate(framerate)
-        assert self.project.framerate == framerate
+        self.project.set_framerate(aeidon.framerates.FPS_25)
+        assert self.project.framerate == aeidon.framerates.FPS_25
         for subtitle in self.project.subtitles:
-            assert subtitle.framerate == framerate
+            assert subtitle.framerate == aeidon.framerates.FPS_25
 
-    @gaupol.deco.reversion_test
+    @aeidon.deco.reversion_test
     def test_shift_positions__frame(self):
-
         orig_subtitles = [x.copy() for x in self.project.subtitles]
         indices = range(len(self.project.subtitles))
         self.project.shift_positions(indices, -10)
@@ -148,9 +149,8 @@ class TestPositionAgent(gaupol.TestCase):
             end = orig_subtitles[i].end_frame - 10
             assert subtitle.end_frame == end
 
-    @gaupol.deco.reversion_test
+    @aeidon.deco.reversion_test
     def test_shift_positions__time(self):
-
         orig_subtitles = [x.copy() for x in self.project.subtitles]
         self.project.shift_positions(None, "00:00:01.000")
         for i, subtitle in enumerate(self.project.subtitles):
@@ -159,9 +159,8 @@ class TestPositionAgent(gaupol.TestCase):
             end = round(orig_subtitles[i].end_seconds + 1.0, 3)
             assert round(subtitle.end_seconds, 3) == end
 
-    @gaupol.deco.reversion_test
+    @aeidon.deco.reversion_test
     def test_shift_positions__seconds(self):
-
         orig_subtitles = [x.copy() for x in self.project.subtitles]
         self.project.shift_positions(None, 1.0)
         for i, subtitle in enumerate(self.project.subtitles):
@@ -170,9 +169,8 @@ class TestPositionAgent(gaupol.TestCase):
             end = round(orig_subtitles[i].end_seconds + 1.0, 3)
             assert round(subtitle.end_seconds, 3) == end
 
-    @gaupol.deco.reversion_test
+    @aeidon.deco.reversion_test
     def test_transform_positions__frame(self):
-
         indices = range(len(self.project.subtitles))
         self.project.transform_positions(indices, (2, 10), (6, 100))
         assert self.project.subtitles[2].start_frame == 10
@@ -180,18 +178,16 @@ class TestPositionAgent(gaupol.TestCase):
             assert 10 < subtitle.start_frame < 100
         assert self.project.subtitles[6].start_frame == 100
 
-    @gaupol.deco.reversion_test
+    @aeidon.deco.reversion_test
     def test_transform_positions__seconds(self):
-
         self.project.transform_positions(None, (2, 20.0), (6, 200.0))
         assert self.project.subtitles[2].start_seconds == 20.0
         for subtitle in self.project.subtitles[3:6]:
             assert 20.0 < subtitle.start_seconds < 200.0
         assert self.project.subtitles[6].start_seconds == 200.0
 
-    @gaupol.deco.reversion_test
+    @aeidon.deco.reversion_test
     def test_transform_positions__time(self):
-
         a, b = "00:00:01.000", "00:00:45.000"
         self.project.transform_positions(None, (2, a), (6, b))
         assert self.project.subtitles[2].start_time == a
