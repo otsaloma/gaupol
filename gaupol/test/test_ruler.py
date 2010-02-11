4ba19a7ffa2f1581f08cd5e933450bbb05866b71
Osmo Salomaa <otsaloma@cc.hut.fi>
2010-02-11 22:47:04 +0200

    Port ruler to gaupol.

--- gaupol/test/test_ruler.py
+++ gaupol/test/test_ruler.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2006-2008 Osmo Salomaa
+# Copyright (C) 2006-2008,2010 Osmo Salomaa
 #
 # This file is part of Gaupol.
 #
@@ -21,13 +21,13 @@ import gtk
 class TestModule(gaupol.TestCase):
 
     def run__text_view(self):
-
         text_view = gtk.TextView()
         text_buffer = text_view.get_buffer()
         text = ("Everything has been said\n"
                 "provided words do not change\n"
                 "their meanings\n"
                 "and meanings their words")
+
         text_buffer.insert_at_cursor(text)
         gaupol.ruler.connect_text_view(text_view)
         scroller = gtk.ScrolledWindow()
@@ -43,40 +43,37 @@ class TestModule(gaupol.TestCase):
         gtk.main()
 
     def test_connect_text_view(self):
-
         text_view = gtk.TextView()
         gaupol.ruler.connect_text_view(text_view)
         text_buffer = text_view.get_buffer()
         text_buffer.insert_at_cursor("test\ntest")
 
     def test_disconnect_text_view(self):
-
         text_view = gtk.TextView()
         gaupol.ruler.connect_text_view(text_view)
         gaupol.ruler.disconnect_text_view(text_view)
         text_buffer = text_view.get_buffer()
         text_buffer.insert_at_cursor("test\ntest")
 
-    def test_get_length_function(self):
-
+    def test_get_length_function__char(self):
         unit = gaupol.length_units.CHAR
         function = gaupol.ruler.get_length_function(unit)
         assert function("iii") == 3
+
+    def test_get_length_function__em(self):
         unit = gaupol.length_units.EM
         function = gaupol.ruler.get_length_function(unit)
         assert 0.5 < function("M") < 1.5
 
     def test_get_lengths__char(self):
-
         unit = gaupol.length_units.CHAR
         gaupol.conf.editor.length_unit = unit
         lengths = gaupol.ruler.get_lengths("MMM\n<i>iii</i>")
-        assert lengths == (3, 3)
+        assert tuple(lengths) == (3, 3)
 
     def test_get_lengths__em(self):
-
         unit = gaupol.length_units.EM
         gaupol.conf.editor.length_unit = unit
         gaupol.conf.editor.use_custom_font = False
         lengths = gaupol.ruler.get_lengths("MMM\n<i>i</i>")
-        assert lengths == (3, 0)
+        assert tuple(lengths) == (3, 0)
