22b9abf395921a2a9dee3897a11d20f754bc8e49
Osmo Salomaa <otsaloma@cc.hut.fi>
2010-04-04 04:25:40 +0300

    Port agents.preview to gaupol.

--- gaupol/agents/preview.py
+++ gaupol/agents/preview.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2005-2008 Osmo Salomaa
+# Copyright (C) 2005-2008,2010 Osmo Salomaa
 #
 # This file is part of Gaupol.
 #
@@ -29,15 +29,13 @@ class PreviewAgent(aeidon.Delegate):
     """Previewing subtitles with a video player."""
 
     def _check_process_state(self, page, process, output_path, command):
-        """Check if the process has terminated or not."""
-
+        """Check if `process` has terminated or not."""
         if process.poll() is None: return True
         self._handle_output(process, output_path, command)
         return False # to not check again.
 
     def _handle_output(self, process, output_path, command):
-        """Handle the output of finished preview process."""
-
+        """Handle output of finished `process`."""
         with open(output_path, "r") as fobj:
             output = fobj.read()
         output = "$ %s\n\n%s" % (command, output)
@@ -49,9 +47,8 @@ class PreviewAgent(aeidon.Delegate):
 
     def _show_encoding_error_dialog(self):
         """Show an error dialog after failing to encode file."""
-
-        title = _('Failed to encode subtitle file to '
-            'temporary directory "%s"') % tempfile.gettempdir()
+        title = _('Failed to encode subtitle file to temporary '
+                  'directory "%s"') % tempfile.gettempdir()
         message = _("Subtitle data could not be encoded to a temporary file "
             "for preview with the current character encoding. Please first "
             "save the subtitle file with a different character encoding.")
@@ -61,19 +58,15 @@ class PreviewAgent(aeidon.Delegate):
 
     def _show_io_error_dialog(self, message):
         """Show an error dialog after failing to write file."""
-
-        title = _('Failed to save subtitle file to '
-            'temporary directory "%s"') % tempfile.gettempdir()
-        message = _("%s.") % message
+        title = _('Failed to save subtitle file to temporary '
+                  'directory "%s"') % tempfile.gettempdir()
         dialog = gaupol.ErrorDialog(self.window, title, message)
         dialog.add_button(gtk.STOCK_OK, gtk.RESPONSE_OK)
         gaupol.util.flash_dialog(dialog)
 
     def _show_process_error_dialog(self, message):
         """Show an error dialog after failing to launch video player."""
-
         title = _("Failed to launch video player")
-        message = _("%s.") % message
         dialog = gaupol.ErrorDialog(self.window, title, message)
         dialog.add_button(gtk.STOCK_OK, gtk.RESPONSE_OK)
         gaupol.util.flash_dialog(dialog)
@@ -81,51 +74,53 @@ class PreviewAgent(aeidon.Delegate):
     @aeidon.deco.export
     def _on_preview_activate(self, *args):
         """Preview from selected position with a video player."""
-
         page = self.get_current_page()
         rows = page.view.get_selected_rows()
         row = (rows[0] if rows else 0)
-        time = page.project.subtitles[row].start_time
+        position = page.project.subtitles[row].start
         col = page.view.get_focus()[1]
         if col == page.view.columns.TRAN_TEXT:
             doc = aeidon.documents.TRAN
         else: # Any other column previews the main file.
             doc = aeidon.documents.MAIN
-        self.preview(page, time, doc)
+        self.preview(page, position, doc)
 
     @aeidon.deco.export
-    def preview(self, page, time, doc, path=None):
-        """Preview from time with a video player."""
-
+    def preview(self, page, position, doc):
+        """Preview from `position` with a video player."""
         command = gaupol.util.get_preview_command()
         offset = gaupol.conf.preview.offset
         encoding = ("utf_8" if gaupol.conf.preview.force_utf_8 else None)
-        preview = page.project.preview
-        args = (time, doc, command, offset, path, encoding)
-        try: process, command, output_path = preview(*args)
+        try: process, command, output_path = page.project.preview(position,
+                                                                  doc,
+                                                                  command,
+                                                                  offset,
+                                                                  encoding)
+
         except aeidon.ProcessError, message:
             return self._show_process_error_dialog(message)
         except (IOError, OSError), (no, message):
             return self._show_io_error_dialog(message)
         except UnicodeError:
             return self._show_encoding_error_dialog()
-        # 'gobject.child_watch_add' does not appear to work on Windows,
+        # glib.child_watch_add does not appear to work on Windows,
         # so let's watch the process by polling it at regular intervals.
-        function = self._check_process_state
-        args = (page, process, output_path, command)
-        gobject.timeout_add(1000, function, *args)
+        gobject.timeout_add(1000,
+                            self._check_process_state,
+                            page,
+                            process,
+                            output_path,
+                            command)
 
     @aeidon.deco.export
     def preview_changes(self, page, row, doc, method, args=None, kwargs=None):
-        """Preview changes caused by method with a video player."""
-
+        """Preview changes caused by `method` with a video player."""
         subtitles = [x.copy() for x in page.project.subtitles]
         framerate = page.project.framerate
         blocked = page.project.block_all()
         method(register=None, *(args or ()), **(kwargs or {}))
-        path = page.project.new_temp_file(doc)
-        time = page.project.subtitles[row].start_time
+        position = page.project.subtitles[row].start
         page.project.unblock_all(blocked)
-        page.project.subtitles = subtitles
+        self.preview(page, position, doc)
         page.project.set_framerate(framerate, register=None)
-        self.preview(page, time, doc, path)
+        page.project.subtitles = subtitles
--- gaupol/agents/test/test_preview.py
+++ gaupol/agents/test/test_preview.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2005-2008 Osmo Salomaa
+# Copyright (C) 2005-2008,2010 Osmo Salomaa
 #
 # This file is part of Gaupol.
 #
@@ -14,121 +14,92 @@
 # You should have received a copy of the GNU General Public License along with
 # Gaupol. If not, see <http://www.gnu.org/licenses/>.
 
-import functools
+import aeidon
 import gaupol
 import gtk
-import os
+import time
 
 
 class TestPreviewAgent(gaupol.TestCase):
 
     def run__show_encoding_error_dialog(self):
-
-        flash_dialog = gaupol.Runner.flash_dialog
-        flash_dialog = functools.partial(flash_dialog, self.application)
-        self.delegate.flash_dialog = flash_dialog
         self.delegate._show_encoding_error_dialog()
 
     def run__show_io_error_dialog(self):
-
-        flash_dialog = gaupol.Runner.flash_dialog
-        flash_dialog = functools.partial(flash_dialog, self.application)
-        self.delegate.flash_dialog = flash_dialog
         self.delegate._show_io_error_dialog("test")
 
     def run__show_process_error_dialog(self):
-
-        flash_dialog = gaupol.Runner.flash_dialog
-        flash_dialog = functools.partial(flash_dialog, self.application)
-        self.delegate.flash_dialog = flash_dialog
         self.delegate._show_process_error_dialog("test")
 
     def setup_method(self, method):
-
         gaupol.conf.preview.custom_command = "echo $SUBFILE"
         gaupol.conf.preview.use_custom_command = True
         self.application = self.new_application()
         page = self.application.get_current_page()
         page.project.video_path = self.new_subrip_file()
         self.delegate = self.application.preview.im_self
-        respond = lambda *args: gtk.RESPONSE_DELETE_EVENT
-        self.delegate.flash_dialog = respond
 
+    @aeidon.deco.monkey_patch(gaupol.util, "flash_dialog")
     def test__show_encoding_error_dialog(self):
-
+        gaupol.util.flash_dialog = lambda *args: gtk.RESPONSE_OK
         self.delegate._show_encoding_error_dialog()
 
+    @aeidon.deco.monkey_patch(gaupol.util, "flash_dialog")
     def test__show_io_error_dialog(self):
-
+        gaupol.util.flash_dialog = lambda *args: gtk.RESPONSE_OK
         self.delegate._show_io_error_dialog("test")
 
+    @aeidon.deco.monkey_patch(gaupol.util, "flash_dialog")
     def test__show_process_error_dialog(self):
-
+        gaupol.util.flash_dialog = lambda *args: gtk.RESPONSE_OK
         self.delegate._show_process_error_dialog("test")
 
-    def test_on_preview_activate(self):
-
+    def test_on_preview_activate__main(self):
         page = self.application.get_current_page()
+        page.view.set_focus(0, page.view.columns.MAIN_TEXT)
         self.application.get_action("preview").activate()
-        for col in filter(page.view.is_text_column, page.view.columns):
-            page.view.set_focus(0, col)
-            self.application.get_action("preview").activate()
-            page.view.select_rows((0, 1, 2))
-            self.application.get_action("preview").activate()
 
-    def test_preview(self):
-
+    def test_on_preview_activate__translation(self):
         page = self.application.get_current_page()
-        time = page.project.subtitles[3].start_time
-        for doc in aeidon.documents:
-            self.application.preview(page, time, doc)
-            path = self.new_subrip_file()
-            self.application.preview(page, time, doc, path)
+        page.view.set_focus(0, page.view.columns.TRAN_TEXT)
+        self.application.get_action("preview").activate()
 
-    def test_preview__io_error(self):
-
+    def test_preview__main(self):
         page = self.application.get_current_page()
-        page.project.remove_subtitles((0,))
-        real_create = aeidon.temp.create
-        temp_paths = []
-        def create_temp(*args):
-            path = real_create(*args)
-            os.chmod(path, 0000)
-            temp_paths.append(path)
-            return path
-        aeidon.temp.create = create_temp
-        time = page.project.subtitles[0].start_time
         doc = aeidon.documents.MAIN
-        self.application.preview(page, time, doc)
-        for path in temp_paths:
-            os.chmod(path, 0777)
-        aeidon.temp.create = real_create
+        self.application.preview(page, 13, doc)
 
-    def test_preview__return_non_zero(self):
-
-        gaupol.conf.preview.custom_command = "/x/y/z.xyz"
+    @aeidon.deco.monkey_patch(gaupol.util, "flash_dialog")
+    def test_preview__process_error(self):
+        gaupol.util.flash_dialog = lambda *args: gtk.RESPONSE_OK
+        gaupol.conf.preview.custom_command = "xxxxx"
         page = self.application.get_current_page()
-        time = page.project.subtitles[0].start_time
         doc = aeidon.documents.MAIN
-        self.application.preview(page, time, doc)
+        self.application.preview(page, 13, doc)
+        time.sleep(1)
+        gaupol.util.iterate_main()
+
+    def test_preview__translation(self):
+        page = self.application.get_current_page()
+        doc = aeidon.documents.TRAN
+        self.application.preview(page, 13, doc)
 
     def test_preview__unicode_error(self):
-
         page = self.application.get_current_page()
-        time = page.project.subtitles[0].start_time
         doc = aeidon.documents.MAIN
         page.project.set_text(0, doc, "\303\266")
         page.project.main_file.encoding = "ascii"
-        self.application.preview(page, time, doc)
+        self.application.preview(page, 13, doc)
 
     def test_preview_changes(self):
-
         page = self.application.get_current_page()
         subtitles = [x.copy() for x in page.project.subtitles]
         framerate = page.project.framerate
-        doc = aeidon.documents.TRAN
-        method = page.project.set_text
-        args = (2, doc, "testing...")
-        self.application.preview_changes(page, 2, doc, method, args)
+        self.application.preview_changes(page,
+                                         3,
+                                         aeidon.documents.TRAN,
+                                         page.project.remove_subtitles,
+                                         ((0, 1, 2),))
+
         assert page.project.subtitles == subtitles
         assert page.project.framerate == framerate
