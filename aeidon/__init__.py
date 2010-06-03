4acf15c5956649da449b8c916b3e76eb995f8cb4
Osmo Salomaa <otsaloma@cc.hut.fi>
2010-06-03 03:39:00 +0300

    RELEASE 0.16

--- aeidon/__init__.py
+++ aeidon/__init__.py
@@ -89,7 +89,7 @@ accessing the subtitles directly (via :attr:`aeidon.Project.subtitles`).
 import os
 import re
 
-__version__ = "0.15.99"
+__version__ = "0.16"
 
 debug = (bool(os.environ.get("AEIDON_DEBUG", "")) or
          bool(os.environ.get("GAUPOL_DEBUG", "")))
--- gaupol/__init__.py
+++ gaupol/__init__.py
@@ -43,7 +43,7 @@ import gobject
 import gtk
 import os
 
-__version__ = "0.15.99"
+__version__ = "0.16"
 
 COMBO_SEPARATOR = "<separator/>"
 
