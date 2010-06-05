663a79599d309bc16f3b8fed5660d39bc0c62ae7
Osmo Salomaa <otsaloma@cc.hut.fi>
2010-06-05 19:25:21 +0300

    Add missing locale directory and fix libenchant dll path.

--- tools/py2exe.bat
+++ tools/py2exe.bat
@@ -10,8 +10,9 @@ xcopy %GTKDIR%\etc dist\etc /s /i /y
 xcopy %GTKDIR%\lib\*.dll dist\lib /i /y
 xcopy %GTKDIR%\lib\gtk-2.0 dist\lib\gtk-2.0 /s /i /y
 xcopy %GTKDIR%\share\icons\hicolor\*.theme dist\share\icons\hicolor /i /y
+xcopy %GTKDIR%\share\locale dist\share\locale /s /i /y
 xcopy %GTKDIR%\share\themes dist\share\themes /s /i /y
 xcopy %PYDIR%\Lib\site-packages\enchant\lib dist\lib /s /i /y
-xcopy %PYDIR%\Lib\site-packages\enchant\libenchant.dll dist /i /y
+xcopy %PYDIR%\Lib\site-packages\enchant\libenchant*.dll dist /i /y
 xcopy %PYDIR%\Lib\site-packages\enchant\share dist\share /s /i /y
 pause
