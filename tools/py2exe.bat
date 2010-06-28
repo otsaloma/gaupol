84456159213e7b78c7b974a7ffc23442c5261748
Osmo Salomaa <otsaloma@cc.hut.fi>
2010-06-28 22:01:23 +0300

    Do not include .dll.a files.

--- tools/py2exe.bat
+++ tools/py2exe.bat
@@ -7,7 +7,6 @@ set PYDIR=%SYSTEMDRIVE%\python26
 %PYDIR%\python.exe winsetup.py py2exe
 xcopy %GTKDIR%\etc dist\etc /s /i /y
 xcopy %GTKDIR%\lib\*.dll dist\lib /s /i /y
-xcopy %GTKDIR%\lib\*.dll.a dist\lib /s /i /y
 xcopy %GTKDIR%\share\icons\hicolor\*.theme dist\share\icons\hicolor /i /y
 xcopy %GTKDIR%\share\locale dist\share\locale /s /i /y
 xcopy %GTKDIR%\share\themes dist\share\themes /s /i /y
