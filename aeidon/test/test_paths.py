cbec802460a1c5d9617c2f3a9cc2991fcc817c0d
Osmo Salomaa <otsaloma@iki.fi>
2011-09-10 21:23:22 +0300

    Port paths to Python 3.

--- aeidon/test/test_paths.py
+++ aeidon/test/test_paths.py
@@ -15,6 +15,7 @@
 # Gaupol. If not, see <http://www.gnu.org/licenses/>.
 
 import aeidon
+import imp
 import os
 import sys
 
@@ -22,90 +23,55 @@ import sys
 class TestModule(aeidon.TestCase):
 
     @aeidon.deco.monkey_patch(sys, "platform")
-    def test__xdg_copy_config_files(self):
-        sys.platform = "linux2"
-        mkdir = aeidon.temp.create_directory
-        rmdir = aeidon.temp.remove_directory
-        aeidon.paths.OBSOLETE_PROFILE_DIR = mkdir()
-        aeidon.paths.CONFIG_HOME_DIR = mkdir()
-        aeidon.paths._xdg_copy_config_files()
-        rmdir(aeidon.paths.OBSOLETE_PROFILE_DIR)
-        rmdir(aeidon.paths.CONFIG_HOME_DIR)
-
-    @aeidon.deco.monkey_patch(sys, "platform")
-    def test__xdg_copy_data_files(self):
-        sys.platform = "linux2"
-        mkdir = aeidon.temp.create_directory
-        rmdir = aeidon.temp.remove_directory
-        aeidon.paths.OBSOLETE_PROFILE_DIR = mkdir()
-        aeidon.paths.DATA_HOME_DIR = mkdir()
-        aeidon.paths._xdg_copy_data_files()
-        rmdir(aeidon.paths.OBSOLETE_PROFILE_DIR)
-        rmdir(aeidon.paths.DATA_HOME_DIR)
-
-    @aeidon.deco.monkey_patch(sys, "platform")
-    def test_config_home_dir__win32(self):
+    def test_config_home_dir__windows(self):
         sys.platform = "win32"
-        reload(aeidon.paths)
+        imp.reload(aeidon.paths)
         assert hasattr(aeidon, "CONFIG_HOME_DIR")
 
     @aeidon.deco.monkey_patch(os, "environ")
     def test_config_home_dir__xdg_environment(self):
         os.environ["XDG_CONFIG_HOME"] = "xdg"
-        reload(aeidon.paths)
+        imp.reload(aeidon.paths)
         assert hasattr(aeidon, "CONFIG_HOME_DIR")
 
     @aeidon.deco.monkey_patch(os, "environ")
     def test_config_home_dir__xdg_no_environment(self):
         os.environ.clear()
-        reload(aeidon.paths)
+        imp.reload(aeidon.paths)
         assert hasattr(aeidon, "CONFIG_HOME_DIR")
 
     @aeidon.deco.monkey_patch(sys, "frozen")
     def test_data_dir__py2exe(self):
         sys.frozen = "py2exe"
-        reload(aeidon.paths)
+        imp.reload(aeidon.paths)
         assert hasattr(aeidon, "DATA_DIR")
 
     def test_data_dir__source(self):
         assert os.path.isdir(aeidon.DATA_DIR)
 
     @aeidon.deco.monkey_patch(sys, "platform")
-    def test_data_home_dir__win32(self):
+    def test_data_home_dir__windows(self):
         sys.platform = "win32"
-        reload(aeidon.paths)
+        imp.reload(aeidon.paths)
         assert hasattr(aeidon, "DATA_HOME_DIR")
 
     @aeidon.deco.monkey_patch(os, "environ")
     def test_data_home_dir__xdg_environment(self):
         os.environ["XDG_DATA_HOME"] = "xdg"
-        reload(aeidon.paths)
+        imp.reload(aeidon.paths)
         assert hasattr(aeidon, "DATA_HOME_DIR")
 
     @aeidon.deco.monkey_patch(os, "environ")
     def test_data_home_dir__xdg_no_environment(self):
         os.environ.clear()
-        reload(aeidon.paths)
+        imp.reload(aeidon.paths)
         assert hasattr(aeidon, "DATA_HOME_DIR")
 
     @aeidon.deco.monkey_patch(sys, "frozen")
     def test_locale_dir__py2exe(self):
         sys.frozen = "py2exe"
-        reload(aeidon.paths)
+        imp.reload(aeidon.paths)
         assert hasattr(aeidon, "LOCALE_DIR")
 
     def test_locale_dir__source(self):
         assert hasattr(aeidon, "LOCALE_DIR")
-
-    @aeidon.deco.monkey_patch(sys, "platform")
-    def test_xdg_copy_if_applicable(self):
-        sys.platform = "linux2"
-        mkdir = aeidon.temp.create_directory
-        rmdir = aeidon.temp.remove_directory
-        aeidon.paths.OBSOLETE_PROFILE_DIR = mkdir()
-        aeidon.paths.CONFIG_HOME_DIR = mkdir()
-        rmdir(aeidon.paths.CONFIG_HOME_DIR)
-        aeidon.paths.DATA_HOME_DIR = mkdir()
-        rmdir(aeidon.paths.DATA_HOME_DIR)
-        aeidon.paths.xdg_copy_if_applicable()
-        rmdir(aeidon.paths.OBSOLETE_PROFILE_DIR)
