09f5769149ecbaa6450c02da3219e5adfeb2a11b
Osmo Salomaa <otsaloma@iki.fi>
2012-10-14 19:35:29 +0300

    RELEASE 0.20

--- gaupol/__init__.py
+++ gaupol/__init__.py
@@ -44,7 +44,7 @@ GTK+ user interface for the Gaupol subtitle editor.
 :var conf: Instance of :class:`gaupol.ConfigurationStore` used
 """
 
-__version__ = "0.19.91"
+__version__ = "0.20"
 COMBO_SEPARATOR = "<separator/>"
 
 try:
