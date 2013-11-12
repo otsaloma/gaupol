d78c9e56e5286c120d2488609e1d0ed4e60afd5f
Osmo Salomaa <otsaloma@iki.fi>
2013-11-13 01:09:55 +0200

    Fix log filename.

--- tools/winbuild.bat
+++ tools/winbuild.bat
@@ -3,7 +3,7 @@ cd "%~dp0\.."
 set GAUPOL_FREEZING=1
 set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;
 set PYTHON=C:\Python33\python.exe
-del /s /q build build.log
+del /s /q build winsetup.log
 %PYTHON% setup.py install_data -d build\usr
 %PYTHON% winsetup.py build > winsetup.log
 :: Remove some of the biggest fucking junk included
