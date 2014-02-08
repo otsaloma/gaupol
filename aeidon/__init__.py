c9946c43b5ce58f867d9d99214cba4f8ad5566d0
Osmo Salomaa <otsaloma@iki.fi>
2014-02-08 16:22:59 +0200

    RELEASE 0.25

--- aeidon/__init__.py
+++ aeidon/__init__.py
@@ -87,7 +87,7 @@ import os
 import re
 import sys
 
-__version__ = "0.24.3"
+__version__ = "0.25"
 
 RUNNING_SPHINX = (sys.argv[0].endswith("autogen.py") or
                   sys.argv[0].endswith("sphinx-build"))
