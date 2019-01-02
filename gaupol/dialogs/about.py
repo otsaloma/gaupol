4feb9067e0df781710fb88c697009005abbdf577
Osmo Salomaa <otsaloma@iki.fi>
2019-01-02 22:52:56 +0200

    Bump copyright years

--- gaupol/dialogs/about.py
+++ gaupol/dialogs/about.py
@@ -39,7 +39,7 @@ class AboutDialog(Gtk.AboutDialog):
         self.set_artists(("Osmo Salomaa <otsaloma@iki.fi>",))
         self.set_authors(("Osmo Salomaa <otsaloma@iki.fi>",))
         self.set_comments(_("Subtitle editor"))
-        self.set_copyright("Copyright © 2005–2018 Osmo Salomaa")
+        self.set_copyright("Copyright © 2005–2019 Osmo Salomaa")
         self.set_license_type(Gtk.License.GPL_3_0)
         self.set_logo_icon_name("gaupol")
         self.set_program_name("Gaupol")
